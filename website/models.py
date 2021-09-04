91c3289a3f542310b6c71d67f9bf49ae3c4070b9
Chino Franco <chino.franco@gmail.com>
2021-09-04 16:48:22 +0900

    Delete website directory

--- website/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from sqlalchemy.sql import func
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.String(150), unique=True)
-    
